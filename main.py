248e0fa86395dee3df62441c9ee17068f98bb44a
Jeremie A <mail@lp1.eu>
2022-09-21 18:25:42 +0200

    added ssh support using Twisted (no logging yet)

--- main.py
+++ main.py
@@ -2,45 +2,59 @@
 
 from logger import Logger
 from select import select
+from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
 from protocols import telnet, smtp, ssh, http, ftp, adb, pop
 
 HOST=""
 LOOP=True
 logger = Logger(open("logs/log.txt", "a+"))
+protos = []
+sockets = []
+
+def     loop():
+    print(loop)
+    r_sockets = []
+    w_sockets = []
+    e_sockets = []
+    
+    for p in protos:
+        r_sockets = r_sockets + p.get_read_sockets()
+        w_sockets = w_sockets + p.get_write_sockets()
+    
+    r, w, e = select(r_sockets, w_sockets, e_sockets)
+    for sock in r:
+        if sock.fileno() in sockets.keys():
+            p = sockets[sock.fileno()]
+            if sock.fileno() == p.socket.fileno():
+                p.accept()
+        else:
+            for p in protos:
+                if sock in p.clients:
+                    p.read(sock)
+    return
 
 def	main():
+    global protos
+    global sockets
     sockets = {}
-    protocols = [
+    # Twisted init
+    ssh.init(HOST, logger)
+
+    # Non-twisted init
+    protos = [
         telnet.init(HOST, logger),
         smtp.init(HOST, logger),
-        ssh.init(HOST, logger),
         http.init(HOST, logger),
         ftp.init(HOST, logger),
         adb.init(HOST, logger),
         pop.init(HOST, logger)
     ]
-    for p in protocols:
+    for p in protos:
         sockets[p.socket.fileno()] = p
-
-    while LOOP:
-        r_sockets = []
-        w_sockets = []
-        e_sockets = []
-        for p in protocols:
-            r_sockets = r_sockets + p.get_read_sockets()
-            w_sockets = w_sockets + p.get_write_sockets()
-    
-        r, w, e = select(r_sockets, w_sockets, e_sockets)
-        for sock in r:
-            if sock.fileno() in sockets.keys():
-                p = sockets[sock.fileno()]
-                if sock.fileno() == p.socket.fileno():
-                    p.accept()
-            else:
-                for p in protocols:
-                    if sock in p.clients:
-                        p.read(sock)
-
+    lc = LoopingCall(loop)
+    lc.start(0.1)
+    reactor.run()
     return 0
 
 if __name__ == '__main__':
--- protocols/ssh.py
+++ protocols/ssh.py
@@ -1,11 +1,80 @@
+import sys
+
+from zope.interface import implementer
 from .protocol import Protocol
 from faker import Faker
 from datetime import datetime
+from twisted.internet import reactor
+from twisted.conch.ssh import factory, keys, session
+from twisted.conch.ssh.transport import SSHServerTransport
+from twisted.conch.ssh import connection, factory, keys, session, userauth
+from twisted.cred.checkers import InMemoryUsernamePasswordDatabaseDontUse
+from twisted.conch.checkers import InMemorySSHKeyDB, SSHPublicKeyChecker
+from twisted.python import components, log
+from twisted.cred import portal
+
+log.startLogging(sys.stderr)
+
+## Implementation examples
+## https://docs.twistedmatrix.com/en/stable/conch/examples/
+
+SERVER_RSA_PRIVATE = "keys/ssh_host_rsa_key"
+SERVER_RSA_PUBLIC = "keys/ssh_host_rsa_key.pub"
+
+PRIMES = {
+    2048: [
+        (
+            2,
+            int(
+                "4217939"
+            ),
+        )
+    ],
+    4096: [
+        (
+            2,
+            int(
+                "4112035938859"
+            ),
+        )
+    ],
+}
+
 
 BANNER= "SSH-2.0-OpenSSH_8.2p1 Ubuntu-4ubuntu0.5\n"
 
+@implementer(portal.IRealm)
+class ExampleRealm:
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        return super(self, avatarId, mind, interfaces)
+
+class ExampleFactory(factory.SSHFactory):
+    protocol = SSHServerTransport
+
+    services = {
+        b"ssh-userauth": userauth.SSHUserAuthServer,
+        b"ssh-connection": connection.SSHConnection,
+    }
+
+    def __init__(self):
+        passwdDB = InMemoryUsernamePasswordDatabaseDontUse(user="password")
+        sshDB = SSHPublicKeyChecker(InMemorySSHKeyDB({b"user": []}))
+        self.portal = portal.Portal(ExampleRealm(), [passwdDB, sshDB])
+
+    def getPublicKeys(self):
+        return {b"ssh-rsa": keys.Key.fromFile(SERVER_RSA_PUBLIC)}
+
+    def getPrivateKeys(self):
+        return {b"ssh-rsa": keys.Key.fromFile(SERVER_RSA_PRIVATE)}
+
+    def getPrimes(self):
+        return PRIMES
+
+def ssh_handler(data):
+    
+    print(data)
+    return f.handle(data)
+
 def init(host, logger):
     f = Faker('templates/ssh.json')
-    p = Protocol('SSH', 22, host, f.handle, logger, BANNER)
-    p.listen()
-    return p
+    reactor.listenTCP(5022, ExampleFactory())
