35724b9182144e974d40fbd45800851ee7a1f123
Ange Gabrielle <angegabrielle@Jeremies-MacBook-Pro.local>
2023-05-16 18:12:09 +0200

    REQUES LOGGER.PY

--- defaultkeys/README.txt
+++
@@ -1,9 +0,0 @@
-You MUST regenerate new keys when using the honeypot, to avoid detection and potential intrusion!!
-
-Create a new directory named "keys" at the root of this repo.
-
-Them, to regenerate the SSH keys, type :
-
-ssh-keygen -f ssh_host_rsa_key
-
-To generate the RSA keys and x509 certs, run the "gencerts.sh" script from within your "keys" directory.
--- logger.py
+++ logger.py
@@ -6,6 +6,11 @@ Custom logger for honeypot
 import io
 from binascii import hexlify
 from datetime import datetime
+import requests
+
+
+
+
 
 class Logger():
 
@@ -17,7 +22,24 @@ class Logger():
     def save(self, line):
         if type(self.dest) == io.TextIOWrapper:
             self.dest.write(line)
+    
+    def send_encrypted_data(self, encrypted_data):
+        url = 'https://192.168.0.173:5000/test'  
+        headers = {'Content-Type': 'text/plain'} 
 
+        try:
+            response = requests.post(url, data=encrypted_data, headers=headers)
+            if response.status_code == 200:
+                print("Données chiffrées envoyées avec succès à https.py.")
+            else:
+                print("Erreur lors de l'envoi des données chiffrées :", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("Erreur lors de la connexion à https.py :", e)         
+
+    
+
+    
+    
     def log(self, protocol, data, addr, level="log"):
         line = None
         try:
@@ -28,6 +50,8 @@ class Logger():
         print(line)
         self.save(line)
 
+        self.send_encrypted_data(data)
+
     def log_raw(self, protocol_name, protocol_port, client_ip, data, level="log"):
         line = None
         try:
@@ -36,7 +60,8 @@ class Logger():
             print(e)
             line = "%s;%s;%s;%s;%s;%s\n" %(level, datetime.now(), protocol_name, protocol_port, client_ip, hexlify(data).decode('UTF-8'))
         print(line)
-        self.save(line)
+        self.save(line) 
+        self.send_encrypted_data(data)
 
     def warn(self, protocol, data, addr):
         self.log(protocol, data, addr, "warn")
